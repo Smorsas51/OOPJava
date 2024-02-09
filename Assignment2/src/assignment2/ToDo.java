0ac05f4b8f304ccd7800fa02ad74c9d1b83eca7a
Smorsas51 <54513723+Smorsas51@users.noreply.github.com>
2024-02-09 09:21:15 +0100

    lade till folder path för todo filen så vi hämtar dess funktioner i module-info.java

--- Assignment2/src/assignment2/ToDo.java
+++ Assignment2/src/assignment2/ToDo.java
@@ -1,6 +1,10 @@
 package assignment2;
 
 
+import se.his.it401g.todo.HomeTask;
+import se.his.it401g.todo.Task;
+import se.his.it401g.todo.TaskListener;
+
 import javax.swing.JFrame;
 import java.awt.*;
 
@@ -8,6 +12,9 @@ import java.awt.*;
 public class ToDo {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+
+        Task task = new HomeTask();
+        frame.add(task.getGuiComponent());
 
         frame.getContentPane().setBackground(Color.darkGray);
 
