1163f80c713ba5979d5e980ff89e5b79b23f0cb4
Smorsas51 <54513723+Smorsas51@users.noreply.github.com>
2024-02-09 08:55:40 +0100

    3

--- Assignment2/src/assignment2/ToDo.java
+++ Assignment2/src/assignment2/ToDo.java
@@ -1,4 +1,19 @@
 package assignment2;
 
+
+import javax.swing.JFrame;
+import java.awt.*;
+
+
 public class ToDo {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+
+        frame.getContentPane().setBackground(Color.darkGray);
+
+        frame.setBounds(100,100,400,100);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
 }
--- Assignment2/src/module-info.java
+++ Assignment2/src/module-info.java
@@ -1,3 +1,6 @@
 module OOPJava {
+    requires java.base;
+    requires java.se;
+    //requires it401g.todo;
     exports assignment2;
 }
