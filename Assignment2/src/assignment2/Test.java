20c7104022402fbdc532de12e71c429b4af0e771
Smorsas51 <54513723+Smorsas51@users.noreply.github.com>
2024-02-12 13:57:10 +0100

    created test

---
+++ Assignment2/src/assignment2/Test.java
@@ -0,0 +1,14 @@
+package assignment2;
+
+import javax.swing.JFrame;
+
+public class Test extends JFrame{
+
+    Test() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500,500);
+        this.setVisible(true);
+
+    }
+}
--- Assignment2/src/assignment2/ToDo.java
+++ Assignment2/src/assignment2/ToDo.java
@@ -2,22 +2,35 @@ package assignment2;
 
 
 import se.his.it401g.todo.HomeTask;
+import se.his.it401g.todo.StudyTask;
 import se.his.it401g.todo.Task;
 
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
 
 
 public class ToDo {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        //JButton button = new JButton();
 
         Task task = new HomeTask();
+
+        /*button.setBounds(200,100,100,50);
+        button.addActionListener(e -> System.out.println("test"));
+
+        button.setFocusable(false);
+*/
+
+
+
+
+
         frame.add(task.getGuiComponent());
 
         frame.getContentPane().setBackground(Color.darkGray);
 
-        frame.setBounds(100,100,400,100);
+        frame.setBounds(300,300,400,100);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
